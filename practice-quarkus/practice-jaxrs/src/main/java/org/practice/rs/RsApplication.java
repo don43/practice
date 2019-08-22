3aca8348c81575e1e35706bc3035b4e77728dce8
don <don43xu@qq.com>
2019-08-22 11:40:29 +0800

    -

--- practice-quarkus/practice-jaxrs/src/main/java/org/practice/rs/RsApplication.java
+++ practice-quarkus/practice-jaxrs/src/main/java/org/practice/rs/RsApplication.java
@@ -15,26 +15,5 @@ import java.util.concurrent.TimeUnit;
 public class RsApplication {
 
     public static void main(String[] args) throws InterruptedException {
-        Config config = new Config();
-        config.setLockWatchdogTimeout(5 * 1000);
-        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
-        RedissonClient redisson = Redisson.create(config);
-
-        RLock lock = redisson.getLock("anyLock");
-        try {
-            System.out.println("lock start ");
-            lock.lock(10, TimeUnit.SECONDS);
-            System.out.println("lock success ");
-            Thread.sleep(15 * 1000);
-            System.out.println("打印就表示有执行");
-        } finally {
-            try {
-                lock.unlock();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            System.out.println("unlock success ");
-        }
-        redisson.shutdown();
     }
 }
